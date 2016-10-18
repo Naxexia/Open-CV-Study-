ac506f1712475cc5540eb2004c515f86f44830a2
dmnnaxxi <dmnnaxxi@DMNM1758.asia.ad.flextronics.com>
2016-10-18 14:50:21 +0800

    Add some source code

--- Chapter1/src/2/2_3.py
+++ Chapter1/src/2/2_3.py
@@ -8,17 +8,18 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("E:\phonePhotos\\1449641309308.jpg" ,cv2.IMREAD_GRAYSCALE)
-num_rows, num_cols = img.shape[:2]
-
-sobel_horizontal = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-sobel_vertical = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
-
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-
+img = cv2.imread("E:\phonePhotos\\1449641309308.jpg")
 cv2.imshow('Original',img)
-cv2.imshow('Sobel horizontal',sobel_horizontal)
-cv2.imshow('Sobel Vertical',sobel_vertical)
-cv2.imshow('laplacian',laplacian)
 
+size = 15
+
+# generating the kernel
+kernel_motion_blur = np.zeros((size,size))
+kernel_motion_blur[int((size-1)/2),:] = np.ones(size)
+kernel_motion_blur = kernel_motion_blur / size
+
+#applying the kernel to the input image
+output = cv2.filter2D(img,-1,kernel_motion_blur)
+
+cv2.imshow('Motion Blur',output)
 cv2.waitKey()
